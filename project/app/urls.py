898adef0a63d7d2074e8d6077dc1804853644127
M. Fatih Karaduman <98664346+mfkrdmn@users.noreply.github.com>
2022-07-12 00:21:25 +0300

    Delete urls.py

--- project/app/urls.py
+++
@@ -1,8 +0,0 @@
-from urllib import request
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name="index"),
-    path('about', views.about, name="about"),
-]
